53bc5e1902cbc561afa6518341e0d7ba9a97a9fa
Symun-Hossain-Shifat <saymonshifat569625@gmail.com>
2026-02-23 15:09:56 -0800

    third commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let InterviewJob =document.getElementById('interview');
 let rejectedJob = document.getElementById('rejected');
 let maincontainer = document.getElementById('cardscontainer');
 const setText = document.getElementById("noText");
+const setText2 = document.getElementById('totalJobfield');
 // catch the Main button
 const allbtn = document.getElementById('button-all');
 const interviewbtn = document.getElementById('button-interview');
@@ -24,7 +25,7 @@ rejectedJob.innerText = rejectedList.length;
 }
 calculateJob();
 
-
+ setText2.innerText = maincontainer.children.length;
 
 function changebtn(id){
   allbtn.classList.remove('bg-blue-600','text-white')
@@ -42,6 +43,8 @@ function changebtn(id){
 mainsection.addEventListener("click",function(event){
   if(event.target.classList.contains("interviewbtn")){
     const parantnode  = event.target.parentNode.parentNode;
+    parantnode.classList.add('interviewcard');
+    parantnode.classList.remove('rejectedcard');
    
     const  Title =  parantnode.querySelector('.title').innerText;
     const  Developer=  parantnode.querySelector('.developer').innerText;
@@ -66,14 +69,15 @@ mainsection.addEventListener("click",function(event){
 
 
 }else if(event.target.classList.contains("rejectbtn")){
-  const secoundParantnode = event.target.parentNode.parentNode;
- 
-  const Title = secoundParantnode.querySelector('.title').innerText;
-  const  Developer = secoundParantnode.querySelector('.developer').innerText;
-  const Remote = secoundParantnode.querySelector('.remote').innerText;
-  const Description = secoundParantnode.querySelector('.description').innerText;
-  secoundParantnode.querySelector('.appliedbtn').innerText = "REJECTED";
-  secoundParantnode.querySelector('.appliedbtn').classList.add("bg-red-200",'text-red-500','font-extrabold');
+  const parantnode = event.target.parentNode.parentNode;
+    parantnode.classList.add('rejectedcard');
+    parantnode.classList.remove('interviewcard');
+  const Title =parantnode.querySelector('.title').innerText;
+  const  Developer = parantnode.querySelector('.developer').innerText;
+  const Remote = parantnode.querySelector('.remote').innerText;
+  const Description = parantnode.querySelector('.description').innerText;
+ parantnode.querySelector('.appliedbtn').innerText = "REJECTED";
+  parantnode.querySelector('.appliedbtn').classList.add("bg-red-200",'text-red-500','font-extrabold');
   const objectData ={
     Title,
     Developer,
@@ -94,33 +98,96 @@ mainsection.addEventListener("click",function(event){
 })
 
 const emptysection =document.getElementById('Emptysection');
+
+
+
+
+
+
 allbtn.addEventListener("click",function(){
   setText.innerText = "";
   emptysection.classList.add('hidden');
  mainsection.classList.remove("hidden");
+ const allcards = maincontainer.querySelectorAll('.parent > div');
+ for(let card of allcards){
+  card.classList.remove('hidden');
+ }
 })
 
 interviewbtn.addEventListener("click",function(){
   setText.innerText = interviewList.length + " of ";
-  if(interviewList.length === 0){
- emptysection.classList.remove('hidden');
- mainsection.classList.add("hidden");
-}
+  
+ const allcards = maincontainer.children;
+ const emptysection =document.getElementById('Emptysection');
+ let hasinterview = false ;
+ for(let card of allcards){
+  const checkvalidity = card.querySelector('.appliedbtn');
+  if(checkvalidity && checkvalidity.innerText.trim() === "INTERVIEW"){
+    card.classList.remove('hidden');
+    hasinterview = true;
+  }else{
+    card.classList.add('hidden');
+  }};
+  if(!hasinterview){
+    emptysection.classList.remove('hidden')
+  }else{
+ emptysection.classList.add('hidden')
+  }
+  
 })
 
 
 rejectedbtn .addEventListener("click",function(){
+
   setText.innerText = rejectedList.length + " of ";
-  if(rejectedList.length === 0){
- emptysection.classList.remove('hidden');
- mainsection.classList.add("hidden");
-}
+  
+ const allcards = maincontainer.children;
+ const emptysection =document.getElementById('Emptysection');
+  let hasrejected = false;
+ for(let  card of allcards){
+ const checkvalidity = card.querySelector('.appliedbtn');
+ if(checkvalidity && checkvalidity.innerText.trim() ===  "REJECTED"){
+  card.classList.remove('hidden');
+  hasrejected = true;
+ }else{
+  card.classList.add("hidden");
+ }};
+ if(!hasrejected){
+  emptysection.classList.remove('hidden');
+ }else{
+  emptysection.classList.add('hidden');
+ }
+  
 })
 
 
 
+// delete the cards
+maincontainer.addEventListener('click',function(event){
+  if(event.target.classList.contains('secondone')){
+   const parentall = event.target.parentNode.parentNode;
+    parentall.remove();
+   
+    calculateJob();
+     setText2.innerText = maincontainer.children.length;
+     const firstcheck = parentall.querySelector('.appliedbtn');
+     if(firstcheck && firstcheck.innerText.trim() === "INTERVIEW" ){
+      if(interviewList.length > 0){
+      interviewList.pop();
+      }
+      calculateJob();
+     }else if(firstcheck && firstcheck.innerText.trim() === "REJECTED"){
+      if(rejectedList.length > 0){
+        rejectedList.pop();
+      }
+      calculateJob();
+     }
+ 
+  }
+  
+})
 
-
+ 
 
 
 
